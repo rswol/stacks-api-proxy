15b957b664f29b760a510b061216fb4c6383d3f5
bowtiedarcticwolf <bowtiedarcticwolf@gmail.com>
2022-07-04 12:45:54 -0700

    Update building docker images

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:17
 
-COPY target/stacks-api-proxy-1.0-SNAPSHOT.jar /opt/stacks-api-proxy/
+COPY stacks-api-proxy-1.0-SNAPSHOT.jar /opt/stacks-api-proxy/
 
 WORKDIR /opt/stacks-api-proxy/
 CMD ["java", "-jar", "stacks-api-proxy-1.0-SNAPSHOT.jar", "server", "/opt/stacks-api-proxy/config/docker.yml"]
--- build-docker-env.sh
+++ build-docker-env.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-
+# build proxy UI
 docker build ui -f ui/Dockerfile -t stacks-api-proxy-ui
-docker build . -f ui/Dockerfile.nginx -t stacks-api-proxy-nginx  
+
+# build proxy UI/API frontend
+docker build ui -f ui/Dockerfile.nginx -t stacks-api-proxy-nginx  
+
+# build proxy
+docker build target -f Dockerfile -t stacks-api-proxy
 
